8991fe47631c4f15c03d0955e2c1344b571d2e1d
Swift Studies <redwhenexcited@icloud.com>
2016-03-17 10:02:41 +0000

    Refactoring for encapsulation

--- README.md
+++ README.md
@@ -1,2 +1,60 @@
 # Duration
 A simple Swift package for measuring and reporting the time taken for operations
+
+## Usage
+
+### Simple Measurements
+
+There are two methods for simple timing measurements. You can insert calls before and after the section of code you wish to measure, for example
+
+	Duration.startMeasurement("Tough Math")
+	
+	yourToughMathStuff()
+	
+	Duration.stopMeasurement()
+	
+Or you can use a block
+
+	Duration.measure("Tough Math"){
+		yourToughMathStuff()
+	}
+	
+Or even
+
+	Duration.measure("Tough Math", block: yourToughMathStuff)
+	
+In all cases (by default) you will get the output (assuming it took 243 milliseconds)
+
+	Tough Math took: 243ms
+	
+If measurements are nested, they will be appropriately indented in the output, for example if `yourToughMath()` made a measurement of part of its code you would see
+
+	Measuring Tough Math:
+		Part 1 took: 100ms
+		Part 2 took: 143ms
+	Tough Math took: 243ms
+	
+### Understanding Performance Deviations
+
+In order to better understand how your code is impacted by other things the system is doing you can get average times and standard deviations for block based measurements by supplying a number of iterations for the block, so
+
+	Duration.measure("Tough Math", iterations: 10, forBlock:myToughMath)
+	
+Would run the block 10 times, taking and reporting the 10  individual measurements and then the average time taken for the block, together with the standard deviation
+
+	Measuring Tough Math
+		Iteration 1 took: 243ms
+		Iteration 2 took: 242ms
+		...
+		Iteration 10 took: 243ms
+	Tough Math Average: 243ms
+	Tough Math STD Dev.: 1ms
+	
+### Stopping Report Generation
+
+Because you may want to stop reporting of measurements in release builds, you can set the `logStyle` variable in order to control the logging behavior
+
+	Duration.logStyle = .None
+	
+Will disable measurement logging. In the future I will extend this library to support logging to a data-structure for subsequent analysis, but at this point there are two valid values `.None` and `.Print` 
+
--- Sources/Duration.swift
+++ Sources/Duration.swift
@@ -1,3 +1,17 @@
+//    Copyright 2016 Swift Studies
+//
+//    Licensed under the Apache License, Version 2.0 (the "License");
+//    you may not use this file except in compliance with the License.
+//    You may obtain a copy of the License at
+//
+//    http://www.apache.org/licenses/LICENSE-2.0
+//
+//    Unless required by applicable law or agreed to in writing, software
+//    distributed under the License is distributed on an "AS IS" BASIS,
+//    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//    See the License for the specific language governing permissions and
+//    limitations under the License.
+
 import Foundation
 
 public typealias MeasuredBlock = ()->Void
@@ -8,8 +22,6 @@ private var depthIndent : String {
     return String(count: depth, repeatedValue: "\t" as Character)
 }
 
-private var timingStack = [(startTime:Double,name:String?,reported:Bool)]()
-
 /// Define different styles of reporting
 public enum MeasurementLogStyle{
     /// Don't measure anything
@@ -19,100 +31,105 @@ public enum MeasurementLogStyle{
     case Print
 }
 
-/// Set to control how measurements are reported
-public var measurementLogStyle = MeasurementLogStyle.Print
+public class Duration{
+    private static var timingStack = [(startTime:Double,name:String,reported:Bool)]()
 
-///
-/// Call before code you wish to track performance of, multiple measurements can be nested
-///
-public func startMeasurement(name:String? = nil){
-    if measurementLogStyle != .None &&  depth > 0 {
-        let containingMeasurement = timingStack.removeLast()
-        if let name = containingMeasurement.name where !containingMeasurement.reported{
-            print("Measuring \(name):")
+
+    /// Set to control how measurements are reported
+    public static var logStyle = MeasurementLogStyle.Print
+
+    ///
+    /// Call before code you wish to track performance of, multiple measurements can be nested
+    ///
+    public static func startMeasurement(name:String){
+        if logStyle != .None &&  depth > 0 {
+            let containingMeasurement = timingStack.removeLast()
+            if !containingMeasurement.reported && logStyle == .Print{
+                print("Measuring \(containingMeasurement.name):")
+            }
+            timingStack.append((containingMeasurement.startTime,containingMeasurement.name,true))
         }
-        timingStack.append((containingMeasurement.startTime,containingMeasurement.name,true))
-    }
-    timingStack.append((NSDate.timeIntervalSinceReferenceDate(),name,false))
-    depth += 1
-}
-
-///
-/// Stops measuring (and reports if a name of the measurement was originally supplied)
-///
-public func stopMeasurement()->Double{
-    let endTime = NSDate.timeIntervalSinceReferenceDate()
-    precondition(depth > 0, "Attempt to stop a measurement when none has been started")
-    
-    let beginning = timingStack.removeLast()
-    
-    depth -= 1
-    
-    let took = endTime - beginning.startTime
-    
-    if let name = beginning.name where measurementLogStyle == .Print {
-        print("\(depthIndent)\(name) took: \(took.milliSeconds)")
+        timingStack.append((NSDate.timeIntervalSinceReferenceDate(),name,false))
+        depth += 1
     }
     
-    return took
-}
-
-///
-///  Calls a particular block mesuring the time taken to complete the block.
-///  If a name is supplied the time take for each iteration will be reported
-///
-public func measure(name:String? = nil, block: MeasuredBlock)->Double{
-    startMeasurement(name)
-    block()
-    return stopMeasurement()
-}
-
-///
-/// Calls a particular block the specified number of times, returning the average
-/// number of seconds it took to complete the code. If a name is supplied the time
-/// take for each iteration will be reported
-///
-public func measure(name:String? = nil,iterations:Int = 10,forBlock block:MeasuredBlock)->Double{
-    precondition(iterations > 0, "Iterations must be a positive integer")
-    
-    var total : Double = 0
-    var samples = [Double]()
-    
-    if let name = name {
-        print("\(depthIndent)Measuring \(name)")
-    }
-    
-    for _ in 0..<iterations{
-        let took = measure(name,block: block)
+    ///
+    /// Stops measuring (and reports if a name of the measurement was originally supplied)
+    ///
+    public static func stopMeasurement()->Double{
+        let endTime = NSDate.timeIntervalSinceReferenceDate()
+        precondition(depth > 0, "Attempt to stop a measurement when none has been started")
         
-        samples.append(took)
+        let beginning = timingStack.removeLast()
         
-        total += took
-    }
-    
-    let mean = total / Double(iterations)
-
-    if let name = name {
-
-        var deviation = 0.0
+        depth -= 1
         
-        for result in samples {
-            
-            let difference = result - mean
-            
-            deviation += difference*difference
+        let took = endTime - beginning.startTime
+        
+        if logStyle == .Print {
+            print("\(depthIndent)\(beginning.name) took: \(took.milliSeconds)")
         }
         
-        let variance = deviation / Double(iterations)
-        
-        print("\(depthIndent)\(name) Average", mean.milliSeconds)
-        print("\(depthIndent)\(name) STD Dev.", variance.milliSeconds)
+        return took
     }
     
-    return mean
+    ///
+    ///  Calls a particular block mesuring the time taken to complete the block.
+    ///  If a name is supplied the time take for each iteration will be reported
+    ///
+    public static func measure(name:String, block: MeasuredBlock)->Double{
+        startMeasurement(name)
+        block()
+        return stopMeasurement()
+    }
+    
+    ///
+    /// Calls a particular block the specified number of times, returning the average
+    /// number of seconds it took to complete the code. If a name is supplied the time
+    /// take for each iteration will be reported
+    ///
+    public static func measure(name:String,iterations:Int = 10,forBlock block:MeasuredBlock)->Double{
+        precondition(iterations > 0, "Iterations must be a positive integer")
+        
+        var total : Double = 0
+        var samples = [Double]()
+        
+        if logStyle == .Print {
+            print("\(depthIndent)Measuring \(name)")
+        }
+        
+        for i in 0..<iterations{
+            let took = measure("Iteration \(i+1)",block: block)
+            
+            samples.append(took)
+            
+            total += took
+        }
+        
+        let mean = total / Double(iterations)
+        
+        if logStyle == .Print {
+            
+            var deviation = 0.0
+            
+            for result in samples {
+                
+                let difference = result - mean
+                
+                deviation += difference*difference
+            }
+            
+            let variance = deviation / Double(iterations)
+            
+            print("\(depthIndent)\(name) Average", mean.milliSeconds)
+            print("\(depthIndent)\(name) STD Dev.", variance.milliSeconds)
+        }
+        
+        return mean
+    }
 }
 
-extension Double{
+private extension Double{
     var milliSeconds : String {
         return String(format: "%03.2fms", self*1000)
     }
